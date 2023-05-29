c7bedfe211148a3ac15f548c91380964763a5cfc
boliangzhu <37938661+boliangzhu@users.noreply.github.com>
2023-05-29 22:42:20 +0800

    Update AppIAT.js

--- AppIAT.js
+++ AppIAT.js
@@ -1,45 +1,45 @@
-define([‘pipAPI’, ‘https://cdn.jsdelivr.net/gh/boliangzhu/iat@main/BaseIAT.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
 		category1 : {
-			name : ‘Chinese’, //Will appear in the data.
+			name : 'Black people', //Will appear in the data.
 			title : {
-				media : {word : ‘Chinese’}, //Name of the category presented in the task.
-				css : {color:’#31940F’,’font-size’:’2em’}, //Style of the category title.
-				height : 4 //Used to position the “Or” in the combined block.
+				media : {word : 'Black people'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
 			}, 
-			stimulusMedia : [ //Stimuli content as PIP’s media objects
-    		    {image : ‘chi1.jpg’}, 
-    			{image : ‘chi2.jpg’}, 
-    			{image : ‘chi3.jpg’}, 
-    			{image : ‘chi4.jpg’}, 
-    			{image : ‘chi5.jpg’}, 
-    			{image : ‘chi6.jpg’}
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : 'black1.jpg'}, 
+    			{image : 'black2.jpg'}, 
+    			{image : 'black3.jpg'}, 
+    			{image : 'black4.jpg'}, 
+    			{image : 'black5.jpg'}, 
+    			{image : 'black6.jpg'}
 			], 
 			//Stimulus css (style)
-			stimulusCss : {color:’#31940F’,’font-size’:’1.8em’}
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 		category2 :	{
-			name : ‘White people’, //Will appear in the data.
+			name : 'White people', //Will appear in the data.
 			title : {
-				media : {word : ‘White people’}, //Name of the category presented in the task.
-				css : {color:’#31940F’,’font-size’:’2em’}, //Style of the category title.
-				height : 4 //Used to position the “Or” in the combined block.
+				media : {word : 'White people'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
 			}, 
-			stimulusMedia : [ //Stimuli content as PIP’s media objects
-    		    {image : ‘euro1.jpg’}, 
-    			{image : ‘euro2.jpg’}, 
-    			{image : ‘euro3.jpg’}, 
-    			{image : ‘euro4.jpg’}, 
-    			{image : ‘euro5.jpg’}, 
-    			{image : ‘euro6.jpg’}			], 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : 'yf1.jpg'}, 
+    			{image : 'yf4.jpg'}, 
+    			{image : 'yf5.jpg'}, 
+    			{image : 'ym2.jpg'}, 
+    			{image : 'ym3.jpg'}, 
+    			{image : 'ym5.jpg'}			], 
 			//Stimulus css
-			stimulusCss : {color:’#31940F’,’font-size’:’1.8em’}
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 
 		base_url : {//Where are your images at?
-			image : ‘https://boliangzhu.github.io/iat/‘
+			image : 'https://baranan.github.io/minno-tasks/images/'
 		} 
 	});
 });
